f6a901eea0b816f93d04f6690344e61c0d4554b8
dirk <dirk.bangel@gmx.de>
2024-10-20 18:44:31 +0200

    fix lib handling

--- build.sh
+++ build.sh
@@ -6,10 +6,8 @@ cd src/ &&
 ../ham/ham init -u https://github.com/kernkonzept/manifest.git &&
 ../ham/ham sync &&
 cd .. &&
-cp -r src/l4rust/ src/l4/pkg/ &&
 make setup &&
 make &&
 mkdir lib &&
+cd lib/ &&
 find ../obj/ -type f | grep "\.rlib" | xargs -i cp {} . 
-
-
