4295025a6b48aad5f00deb95d536d6815409bb6b
dirk <dirk.bangel@gmx.de>
2024-10-20 08:58:43 +0200

    Add Rust library

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@ git clone https://github.com/kernkonzept/ham.git &&
 cd ham &&
 make &&
 cd .. &&
+mkdir src &&
 cd src/ &&
 ../ham/ham init -u https://github.com/kernkonzept/manifest.git &&
 ../ham/ham sync &&
 cd .. &&
 cp -r src/l4rust/ src/l4/pkg/ &&
 make setup &&
-make 
+make &&
+mkdir lib &&
+find ../obj/ -type f | grep "\.rlib" | xargs -i cp {} . 
+
+
