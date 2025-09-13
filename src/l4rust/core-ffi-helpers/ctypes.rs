4935b835e1c9de7273e0f1b254f79aa445961fea
BDUG <dirk.bangel@gmx.de>
2025-09-13 14:36:53 +0200

    Use libc types unconditionally and test alignment

--- src/l4rust/core-ffi-helpers/ctypes.rs
+++ src/l4rust/core-ffi-helpers/ctypes.rs
@@ -3,26 +3,47 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
-#[cfg(target_arch = "aarch64")]
-mod aarch64 {
-    pub type c_uchar = u8;
-    pub type c_char = i8;
-    pub type c_schar = i8;
-    pub type c_short = i16;
-    pub type c_ushort = u16;
-    pub type c_int = i32;
-    pub type c_uint = u32;
-    pub type c_long = i64;
-    pub type c_ulong = u64;
-    pub type c_longlong = i64;
-    pub type c_ulonglong = u64;
-    // we want to avoid the feature hassle with core vs std., so define our own type
-    pub type c_void = core::ffi::c_void;
+pub use libc::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use core::mem::{align_of, size_of};
+
+    #[test]
+    fn c_types_match_l4re_headers() {
+        assert_eq!(size_of::<c_uchar>(), 1);
+        assert_eq!(align_of::<c_uchar>(), 1);
+
+        assert_eq!(size_of::<c_char>(), 1);
+        assert_eq!(align_of::<c_char>(), 1);
+
+        assert_eq!(size_of::<c_schar>(), 1);
+        assert_eq!(align_of::<c_schar>(), 1);
+
+        assert_eq!(size_of::<c_short>(), 2);
+        assert_eq!(align_of::<c_short>(), 2);
+
+        assert_eq!(size_of::<c_ushort>(), 2);
+        assert_eq!(align_of::<c_ushort>(), 2);
+
+        assert_eq!(size_of::<c_int>(), 4);
+        assert_eq!(align_of::<c_int>(), 4);
+
+        assert_eq!(size_of::<c_uint>(), 4);
+        assert_eq!(align_of::<c_uint>(), 4);
+
+        assert_eq!(size_of::<c_long>(), 8);
+        assert_eq!(align_of::<c_long>(), 8);
+
+        assert_eq!(size_of::<c_ulong>(), 8);
+        assert_eq!(align_of::<c_ulong>(), 8);
+
+        assert_eq!(size_of::<c_longlong>(), 8);
+        assert_eq!(align_of::<c_longlong>(), 8);
+
+        assert_eq!(size_of::<c_ulonglong>(), 8);
+        assert_eq!(align_of::<c_ulonglong>(), 8);
+    }
 }
 
-#[cfg(target_arch = "aarch64")]
-pub use aarch64::*;
-#[cfg(target_arch = "x86_64")]
-use l4re_libc as libc;
-#[cfg(target_arch = "x86_64")]
-pub use libc::*;
