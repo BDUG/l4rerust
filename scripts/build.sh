060a3278d1c43ac1214783ea6039d60a5945742b
BDUG <dirk.bangel@gmx.de>
2025-09-14 07:43:37 +0200

    Log entry and exit in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Entering build container"
+trap 'echo "Leaving build container"' EXIT
+
 SCRIPT_DIR="$(dirname "$0")"
 # shellcheck source=common_build.sh
 source "$SCRIPT_DIR/common_build.sh"
